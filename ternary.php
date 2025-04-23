b6d08f72ee215ade5d99fd005bba517b2e9662fa
fahri <fahrimuhamadfahriansyah@gmail.com>
2025-04-23 14:06:22 +0700

    fahri

--- ternary.php
+++ ternary.php
@@ -7,8 +7,7 @@ $pendapatan = 12000000;  // pendapatan yang diterima setelah investasi
 $status = ($pendapatan > $modal) ? "Untung" :           //  jika pendapatan lebih besar dari modal maka akan mengirimkan "untung"
          (($pendapatan < $modal) ? "Rugi" : "Impas");   //  jika pendapatan lebih kecil dari modal maka akan mengirimkan "rugi"
                                                         //  jika pendapatan dan modal sama maka akan mengirimkan "impas"
-
-                                                        
+                                                      
 echo "Status: $status";  // Outputnya bisa "Untung", "Rugi", atau "Impas"
 ?>
 
